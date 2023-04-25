5d300052e0d3ecf06632fb3c75649df7b08a8377
siddhu003 <113658076+siddhu003@users.noreply.github.com>
2023-04-25 21:43:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 app.use('/users',routes)
 const PORT = process.env.PORT || 5000
 
-const DATABASE_URL= "mongodb+srv://120cs0033:03032003@cluster0.cctwcdu.mongodb.net/test"
+const DATABASE_URL= process.env.CONNECTION_URL
 
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`) }))
